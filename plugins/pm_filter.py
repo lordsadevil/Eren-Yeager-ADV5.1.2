c941017e8c2c351a7d57f1fadb4c074d54fdbfe4
LordSA <96365910+LordSA@users.noreply.github.com>
2022-08-18 12:16:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -93,21 +93,21 @@ async def next_page(bot, query):
     if n_offset == 0:
         btn.insert(0,
             [
-             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total}", 'reqst11'),
+             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total_results}", 'reqst11'),
              InlineKeyboardButton(f"『𝚃𝙸𝙿𝚂』", 'tips')
             ]
         )
     elif off_set is None:
         btn.insert(0,
             [
-             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total}", 'reqst11'),
+             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total_results}", 'reqst11'),
              InlineKeyboardButton(f"『𝚃𝙸𝙿𝚂』", 'tips')
             ]
         )
     else:
         btn.insert(0,
             [
-             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total}", 'reqst11'),
+             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total_results}", 'reqst11'),
              InlineKeyboardButton(f"『𝚃𝙸𝙿𝚂』", 'tips')
             ]
         )
@@ -1228,14 +1228,14 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.insert(0,
             [
-             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total}", 'reqst11'),
+             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total_results}", 'reqst11'),
              InlineKeyboardButton(f"『𝚃𝙸𝙿𝚂』", 'tips')
             ]
         )
     else:
        btn.insert(0,
             [
-             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total}", 'reqst11'),
+             InlineKeyboardButton(f"『𝙵𝙸𝙻𝙴𝚂』: {total_results}", 'reqst11'),
              InlineKeyboardButton(f"『𝚃𝙸𝙿𝚂』", 'tips')
             ]
         )
